7c2e0828b64f26136ccce2ca2485ae6fbf704237
horry <horry@pop-os.localdomain>
2024-06-17 21:17:41 +0200

    some vfx

---
+++ effects/flame.gd
@@ -0,0 +1,15 @@
+@tool
+extends Node3D
+
+@onready var smoke: GPUParticles3D = $Smoke
+@onready var flame: GPUParticles3D = $Flame
+@onready var sparks: GPUParticles3D = $Sparks
+
+@export var smoke_color:  Color = Color.BLACK
+@export var flame_color:  Color = Color.RED
+@export var sparks_color: Color = Color.YELLOW_GREEN
+
+func _process(_delta):
+	smoke.process_material.color = smoke_color
+	flame.process_material.color = flame_color
+	smoke.process_material.color = smoke_color
